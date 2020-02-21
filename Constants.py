8d3b68a7db8d490fdae3ae821e8113128abb0e68
PlasticBagYeet <bonnyg@cox.net>
2020-02-21 11:04:05 -0800

    adding new images

---
+++ Constants.py
@@ -0,0 +1,14 @@
+import os
+import pygame
+from pygame.locals import Rect
+
+current_directory = os.getcwd()
+home_path = os.environ._data["HOMEDRIVE"] + os.environ.data["HOMEPATH"]
+
+DISPLAY_WIDTH = 1024
+DISPLAY_HEIGHT = 768
+GOLD = (255, 215, 0)
+IMAGES_PATH = "data\\images"
+SCREEN_RECTANGLE = Rect(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT)
+STAR_JEDI_LARGE_FONT home_path + "\\AppData\\Local\\Microsoft\\Windows\\Fonts\\Starjedi.ttf"
+MAIN_DIRECTORY =os.path.split(os.path.abspath(__file__))[0]
--- StarWarsDog.py
+++ StarWarsDog.py
@@ -2,3 +2,5 @@ import pygame
 
 class StarWarsDog:
     show_cut_scene = True
+    all_game_rects = pygame.sprite.RenderUpdates()
+    cut_scene_background = pygame .Surface(Constants.SCREEN_RECTANGLE.size)
