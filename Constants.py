beba1ebb8f4a027846b15d225b2c80db6adb676f
PlasticBagYeet <bonnyg@cox.net>
2020-03-06 09:58:46 -0800

    updates

--- Constants.py
+++ Constants.py
@@ -3,12 +3,12 @@ import pygame
 from pygame.locals import Rect
 
 current_directory = os.getcwd()
-home_path = os.environ._data["HOMEDRIVE"] + os.environ.data["HOMEPATH"]
+home_path = os.environ._data["HOMEDRIVE"] + os.environ._data["HOMEPATH"]
 
 DISPLAY_WIDTH = 1024
 DISPLAY_HEIGHT = 768
 GOLD = (255, 215, 0)
 IMAGES_PATH = "data\\images"
 SCREEN_RECTANGLE = Rect(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT)
-STAR_JEDI_LARGE_FONT home_path + "\\AppData\\Local\\Microsoft\\Windows\\Fonts\\Starjedi.ttf"
+STAR_JEDI_LARGE_FONT = home_path + "\\AppData\\Local\\Microsoft\\Windows\\Fonts\\Starjedi.ttf"
 MAIN_DIRECTORY =os.path.split(os.path.abspath(__file__))[0]
--- StarWarsDog.py
+++ StarWarsDog.py
@@ -1,6 +1,18 @@
 import pygame
+import Constants
 
 class StarWarsDog:
     show_cut_scene = True
     all_game_rects = pygame.sprite.RenderUpdates()
     cut_scene_background = pygame .Surface(Constants.SCREEN_RECTANGLE.size)
+
+    def main(self):
+        if(pygame.get_sdl_version()[0] ==2):
+
+        pygame.init()
+
+
+
+
+if(__name__== "__main__"):
+    StarWarsDog().main()
